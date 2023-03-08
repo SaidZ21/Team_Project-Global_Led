f0f6d13984e051887dd37474f29459619fe722c8
Said <Saidqwerty1221@gmail.com>
2023-03-08 21:32:41 +0300

    point in map

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,8 +29,8 @@ export default function NavBar({ user }) {
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
             <a className="nav-link active" href="/">Главная</a>
-            <a className="nav-link active" href="/">Услуги</a>
-            <a className="nav-link active" href="/">Отзывы</a>
+            <a className="nav-link active" href="/servises">Услуги</a>
+            <a className="nav-link active" href="/review">Отзывы</a>
             <a className="nav-link active" href="/contacts">Контакты</a>
             {/* <a className="nav-link" href="/login">Войти</a> */}
             <div className="collapse navbar-collapse" id="navbarNavDarkDropdown" />
--- src/server.js
+++ src/server.js
@@ -4,19 +4,20 @@ import { pathMiddleware } from './middlewares';
 import customRender from './utils/customRender';
 import renderRouter from './routes/renderRouter';
 import servisesRouter from './routes/servisesRouter';
+import reviewRouter from './routes/reviewRouter';
+import ContactRouter from './routes/ContactRouter';
 
-
-require("dotenv").config();
+require('dotenv').config();
 
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
 
-app.engine("jsx", customRender);
-app.set("views", path.join(__dirname, "components"));
-app.set("view engine", "jsx");
+app.engine('jsx', customRender);
+app.set('views', path.join(__dirname, 'components'));
+app.set('view engine', 'jsx');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.json());
 
 // app.use(session({
@@ -30,8 +31,10 @@ app.use(express.json());
 app.use(pathMiddleware);
 
 app.use('/', renderRouter);
+app.use('/contacts', ContactRouter);
 app.use('/servises', servisesRouter);
+app.use('/review', reviewRouter);
 
 app.listen(PORT, () => {
-  console.log("server start on port ", PORT);
+  console.log('server start on port ', PORT);
 });
