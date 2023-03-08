0dd49bfb5f28caffa902f8023adec093db5d0cc3
yevgen237 <yevgen237@gmail.com>
2023-03-08 16:37:13 +0300

    review create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './MainPage';
 import NavBar from './Navbar';
+import Review from './review/Review';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </div>
   );
