89931d66bb39c004f0872cf13f9f394d921c3050
Said <Saidqwerty1221@gmail.com>
2023-03-10 14:57:20 +0300

    delete and change

--- src/components/review/OneReview.jsx
+++ src/components/review/OneReview.jsx
@@ -1,8 +1,16 @@
+import axios from 'axios';
+import React, { useState } from 'react';
 
-import React, { useState } from "react";
+export default function OneReview({
+  review, changeHandler, setReview, user,
+}) {
+  const deleteHandler = () => {
+    axios.delete(`/review/${review.id}`)
+      .then(() => setReview((prev) => prev.filter((onepost) => onepost.id !== post.id)))
+      .catch(console.log);
+    window.location.reload();
+  };
 
-
-export default function OneReview({ review,  changeHandler }) {
   return (
     <div className="card m-3" style={{ width: '500px' }}>
       <div className="row g-0">
@@ -13,9 +21,21 @@ export default function OneReview({ review,  changeHandler }) {
           <div className="card-body">
             <h5 className="card-title">{review.name}</h5>
             <p className="card-text">{review.title}</p>
-            <button href="#" onClick={() => changeHandler(review.id)} class="btn btn-primary">
+            <button href="#" onClick={() => changeHandler(review.id)} className="btn btn-primary">
               Подробнее
             </button>
+
+            {(user?.isAdmin || user) ? (
+              <button
+                className="btn btn-danger"
+                onClick={deleteHandler}
+              >
+                Удалить
+
+              </button>
+            ) : (
+              null)}
+
             <p className="card-text"><small className="align-bottom">Last updated 3 mins ago</small></p>
           </div>
         </div>
